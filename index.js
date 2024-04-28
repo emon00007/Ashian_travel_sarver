b9f47a386c55904dab6b2bba58c2e52539c71013
emon00007 <skemon152@gmail.com>
2024-04-28 15:51:42 +0600

    data post completed

--- index.js
+++ index.js
@@ -25,6 +25,14 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const touristSpotCollection= client.db('addTouristSpotDB').collection('addTouristSpot')
+    app.post('/addTouristSpot',async(req,res)=>{
+        const newAddTouristSpot =req.body 
+        console.log(newAddTouristSpot)
+        const result =await touristSpotCollection.insertOne(newAddTouristSpot)
+        res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
